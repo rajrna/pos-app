9e123c965f7270b3d3f4339145eb94bb5fbba945
Raj Rana <be2020se715@gces.edu.np>
2026-02-11 15:35:33 +0545

    minor change

--- hooks/useCustomers.ts
+++ hooks/useCustomers.ts
@@ -1,31 +1,31 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
+// import {
+//   useQuery,
+//   useMutation,
+//   useQueryClient,
+// } from "@tanstack/react-query";
 
-import type {
-  Customer,
-  CreateCustomerInput,
-} from "@/types/customer";
+// import type {
+//   Customer,
+//   CreateCustomerInput,
+// } from "@/types/customer";
 
-import { fetchCustomers } from "@/services/apiUser";
+// import { fetchCustomers } from "@/services/apiUser";
 
-// Queries
-export function useCustomers(
-  searchQuery?: string,
-) {
-  return useQuery({
-    queryKey: customerKeys.list(searchQuery),
-    queryFn: () => fetchCustomers(searchQuery),
-    staleTime: 30 * 1000, // 30 seconds
-  });
-}
+// // Queries
+// export function useCustomers(
+//   searchQuery?: string,
+// ) {
+//   return useQuery({
+//     queryKey: customerKeys.list(searchQuery),
+//     queryFn: () => fetchCustomers(searchQuery),
+//     staleTime: 30 * 1000, // 30 seconds
+//   });
+// }
 
-export function useCustomer(id: string) {
-  return useQuery({
-    queryKey: customerKeys.detail(id),
-    queryFn: () => fetchCustomerById(id),
-    enabled: !!id,
-  });
-}
+// export function useCustomer(id: string) {
+//   return useQuery({
+//     queryKey: customerKeys.detail(id),
+//     queryFn: () => fetchCustomerById(id),
+//     enabled: !!id,
+//   });
+// }
