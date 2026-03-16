532969f1753517dc83185bc3f79b0df159682297
Raj Rana <be2020se715@gces.edu.np>
2026-03-16 12:07:08 +0545

    Added heatmap

--- app/(app)/dashboard/(overview)/heatmap/page.tsx
+++ app/(app)/dashboard/(overview)/heatmap/page.tsx
@@ -1,3 +1,9 @@
+import Heatmap from "@/components/dashboard/heatmap/Heatmap";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="w-full px-4">
+      <Heatmap />
+    </div>
+  );
 }
